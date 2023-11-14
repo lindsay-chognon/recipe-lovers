90ecdd51b3eafdd4cefcb38ce058f07f1b657b6c
lindsay <lindsaychognon@gmail.com>
2023-11-14 20:06:48 +0100

    Pagination ingredients

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,10 +12,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'ingredient')]
-    public function index(IngredientRepository $repository): Response
+    public function index(
+        IngredientRepository $repository, 
+        PaginatorInterface $paginator,
+        Request $request
+        ): Response
     {
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('pages/ingredient/index.html.twig', [
-            'ingredients' => $repository->findAll()
+            'ingredients' => $ingredients
         ]);
     }
 }
