1cfa81a32a5f61ca4526ff36573d99e196707b81
lindsay <lindsaychognon@gmail.com>
2023-11-21 19:25:22 +0100

    Process the ingredient form

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Ingredient;
 use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
@@ -41,10 +42,32 @@ class IngredientController extends AbstractController
     }
 
     #[Route('/ingredient/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
-    public function new(): Response {
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+        ): Response {
 
         $ingredient = new Ingredient();
         $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+
+        // if the form is submit and valid comparated to different constraints in IngredientType
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été créé avec succès !'
+            );
+        } else {
+            $this->addFlash(
+                'warning',
+                'Il y a un problème.'
+            );
+        }
 
         return $this->render('pages/ingredient/new.html.twig', [
             'form' => $form->createView()
