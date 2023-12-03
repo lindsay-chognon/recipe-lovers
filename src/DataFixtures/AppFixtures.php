1801abeb654bd2709bf7d8aad5112f84238e4030
lindsay <lindsaychognon@gmail.com>
2023-12-03 12:40:32 +0100

    Fixtures for recipe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -22,16 +23,46 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void {
 
+        // ingredients
+        // put ingredients in array to use in recipes
+        $ingredients = [];
         for ($i = 0; $i <= 50; $i++) { 
             $ingredient = new Ingredient();
             $ingredient
             ->setName($this->faker->word())
             ->setPrice(mt_rand(1, 100))
-        ;
+            ;
 
+        $ingredients[] = $ingredient;
         $manager->persist($ingredient);
 
         }
+
+        // recipes
+        for ($j = 0; $j < 25; $j++) { 
+            $recipe = new Recipe();
+            $recipe
+            ->setName($this->faker->word())
+            // time could be null
+            // so if time is 1, mt_rand for time
+            // else time is null
+            ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
+            ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
+            ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
+            ->setDescription($this->faker->text(300))
+            ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+            ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false)
+            ;
+
+            // for one recipe add between 5 and 15 ingredients
+            for ($k=0; $k < mt_rand(5, 15); $k++) { 
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+            }
+
+            $manager->persist($recipe);
+
+        }
+
         
         $manager->flush();
     }
