6b6f868a9dff2a736f11b9afb5c1d89f3400742d
lindsay-chognon <lindsaychognon@gmail.com>
2023-12-05 14:03:50 +0100

    Add recipe controller with index method and view

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+
+class RecipeController extends AbstractController
+{
+    /**
+     * Display all recipes with pagination
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/recette', name: 'recipe', methods: ['GET'])]
+    public function index(
+        RecipeRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+        ): Response
+    {
+
+        $recipes = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+        return $this->render('pages/recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+}
