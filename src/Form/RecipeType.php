e1b503f568fba513af60b7287dc5bdb4da27b440
lindsay <lindsaychognon@gmail.com>
2023-12-10 10:54:34 +0100

    Debug checkbox display

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -122,12 +122,12 @@ class RecipeType extends AbstractType
             ])  
             ->add('isFavorite', CheckboxType::class, [
                 'attr' => [
-                    'class' => 'form-control'
+                    'class' => 'form-check-input'
                 ],
                 'required' => false,
                 'label' => 'Favoris ? ',
                 'label_attr' => [
-                    'class' => 'form-label mt-4'
+                    'class' => 'form-check-label'
                 ],
                 'constraints' => [
                     new Assert\NotNull(),
@@ -147,6 +147,7 @@ class RecipeType extends AbstractType
                 'multiple' => true,
                 'expanded' => true
             ])
+            // create / edit according to the situation
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
