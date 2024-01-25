20d03308508ccf09e7c6aae00d3903694a62e047
lindsay <lindsaychognon@gmail.com>
2024-01-25 18:10:28 +0100

    Proceed user edit (without password edit for the moment)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,14 +4,29 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-    #[Route('/utilisateur/edition/{id}', name: 'user.edit')]
-    public function edit(User $user): Response
+
+    /**
+     * Edit user profile
+     *
+     * @param User $user
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
+    public function edit(
+        User $user, 
+        Request $request,
+        EntityManagerInterface $manager
+        ): Response
     {
         // we check is user is logged
         if (!$this->getUser()) {
@@ -24,6 +39,22 @@ class UserController extends AbstractController
         }
 
         $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Les informations de votre compte ont bien été modifiées.'
+            );
+
+
+            return $this->redirectToRoute('recipe');
+        }
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
