4833234f044976d2f00b5c2a3b237062fe0327ab
lindsay-chognon <lindsaychognon@gmail.com>
2024-01-30 14:26:29 +0100

    User password checking for accound edit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,25 +10,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class UserController extends AbstractController
 {
-
-    /**
-     * Edit user profile
-     *
-     * @param User $user
-     * @param EntityManagerInterface $manager
-     * @param Request $request
-     * @return Response
-     */
-    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit')]
     public function edit(
-        User $user, 
+        User $user,
         Request $request,
         EntityManagerInterface $manager,
         UserPasswordHasherInterface $hasher
-        ): Response
+    ): Response
     {
         // we check is user is logged
         if (!$this->getUser()) {
@@ -43,30 +35,22 @@ class UserController extends AbstractController
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-
-            // User must fill his own password to edit profile
-            if ($hasher->isPasswordValid($user,$form->getData()->getPlainPassword())) {
+            if ($hasher->isPasswordValid($user, $form->getData()->getPlainPassword())) {
                 $user = $form->getData();
                 $manager->persist($user);
                 $manager->flush();
-    
+
                 $this->addFlash(
                     'success',
-                    'Les informations de votre compte ont bien été modifiées.'
+                    'Vos informations ont bien été modifiées.'
                 );
-    
-                return $this->redirectToRoute('recipe');
-
             } else {
                 $this->addFlash(
-                    'warning',
-                    'Le mot de passe renseigné est incorrect.'
+                  'warning',
+                  'Le mot de passe renseigné est incorrect.'
                 );
             }
-
-           
         }
 
         return $this->render('pages/user/edit.html.twig', [
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,13 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
@@ -47,6 +46,7 @@ class UserType extends AbstractType
                     new Assert\Length(['min' => 2, 'max' => 50])
                 ]
             ])
+
             ->add('plainPassword', PasswordType::class, [
                 'attr' => [
                     'class' => 'form-control'
@@ -56,6 +56,8 @@ class UserType extends AbstractType
                     'class' => 'form-label  mt-4'
                 ]
             ])
+
+
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
