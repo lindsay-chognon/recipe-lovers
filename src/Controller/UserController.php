94514998489c6969fee18e81d904b9805902340d
lindsay-chognon <lindsaychognon@gmail.com>
2024-01-31 14:25:26 +0100

    Edit user password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserPasswordType;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -55,6 +56,47 @@ class UserController extends AbstractController
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/utilisateur/edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher
+    ) : Response {
+
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setPassword(
+                    $hasher->hashPassword($user, $form->getData()['newPassword'])
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Votre mot de passe a bien été modifié.'
+                );
+
+                return $this->redirectToRoute('recipe');
+            } else {
+
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe est incorrect.'
+                );
+            }
+        }
+
+        return $this->render('pages/user/edit_password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
