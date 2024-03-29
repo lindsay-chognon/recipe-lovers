ba2ad792076f59e1a88d59f0ba79f85c27b92925
lindsay-chognon <lindsaychognon@gmail.com>
2024-03-29 14:12:34 +0100

    Repo method findPublicRecipes

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -21,4 +21,22 @@ class RecipeRepository extends ServiceEntityRepository
         parent::__construct($registry, Recipe::class);
     }
 
+    /**
+     * Find public recipes according to number asked
+     * @param int $nbRecipes
+     * @return array
+     */
+    public function findPublicRecipes(?int $nbRecipes) : array {
+        $queryBuilder = $this->createQueryBuilder('r')
+            ->where('r.isPublic = 1')
+            ->orderBy('r.createdAt', 'DESC');
+
+        if ($nbRecipes !== 0 || $nbRecipes === null) {
+            $queryBuilder->setMaxResults($nbRecipes);
+        }
+
+        return $queryBuilder->getQuery()
+            ->getResult();
+    }
+
 }
