ee7541d42f69b960c3faf24d8a472398f62bfc39
lindsay-chognon <lindsaychognon@gmail.com>
2024-06-06 14:34:43 +0200

    Magic method for createdAt

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -37,6 +37,10 @@ class Contact
     #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
 
+    public function __construct() {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
