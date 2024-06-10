601f14531b9d120fe8d14f59989d586880079b54
lindsay-chognon <lindsaychognon@gmail.com>
2024-06-10 14:17:10 +0200

    Process contact form & redirect success

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,38 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contact', name: 'contact.index')]
+    public function index(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé.'
+            );
+
+            return $this->redirectToRoute('contact.index');
+        }
         return $this->render('pages/contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
