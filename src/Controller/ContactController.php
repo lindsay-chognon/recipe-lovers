0163eefd585201ea22c1f6775c4d6933befa9b03
lindsay-chognon <lindsaychognon@gmail.com>
2024-06-17 13:57:46 +0200

    Sending contact email (to divide in another special method)

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,14 +8,21 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/contact', name: 'contact')]
     public function index(
         Request $request,
-        EntityManagerInterface $manager
+        EntityManagerInterface $manager,
+        MailerInterface $mailer
     ): Response
     {
         $contact = new Contact();
@@ -35,12 +42,26 @@ class ContactController extends AbstractController
 
             $manager->flush();
 
+            // email
+            $email = (new Email())
+                ->from('hello@example.com')
+                ->to('you@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
+
             $this->addFlash(
                 'success',
                 'Votre message a bien été envoyé.'
             );
 
-            return $this->redirectToRoute('contact.index');
+            return $this->redirectToRoute('contact');
         }
         return $this->render('pages/contact/index.html.twig', [
             'form' => $form->createView(),
