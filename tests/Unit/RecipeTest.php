c0ffe02018b003c63761cf83d2492ae89bf9ddd9
lindsay-chognon <lindsaychognon@gmail.com>
2024-07-09 17:44:43 +0200

    Unit test for assert invalid name of Recipe

--- tests/Unit/RecipeTest.php
+++ tests/Unit/RecipeTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit;
 
+use App\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class RecipeTest extends KernelTestCase
@@ -24,4 +25,20 @@ class RecipeTest extends KernelTestCase
 
     }
 
+    public function testInvalidName() {
+        self::bootKernel();
+        $container = static::getContainer();
+        $recipe = new Recipe();
+        $recipe->setName('')
+            ->setDescription('Description 1')
+            ->setIsFavorite(true)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable());
+
+
+        $errors = $container->get('validator')->validate($recipe);
+
+        $this->assertCount(2, $errors);
+
+    }
 }
