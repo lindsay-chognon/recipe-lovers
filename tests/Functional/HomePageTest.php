6fbe81e7a8212ac9257cc6621a2b65756d7d5e7a
lindsay-chognon <lindsaychognon@gmail.com>
2024-07-09 18:12:45 +0200

    Tests functional tests (btn registration)

---
+++ tests/Functional/HomePageTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomePageTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        // create a client link a web browser
+        $client = static::createClient();
+        // request return a crawler for more complete tests
+        // Make requests on DOM
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+
+        $button = $crawler->filter('.btn');
+        $this->assertEquals(1, count($button));
+
+        $this->assertSelectorTextContains('h1', 'Bienvenue');
+    }
+}
